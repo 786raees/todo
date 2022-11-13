e2f017bf2386f7a8168bcfd34c3cb5b52d9e0e71
Waqar Khan <sms.raees@gmail.com>
2022-11-13 21:16:58 +0500

    added update

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="home_view"),
-    path('delete_todo/<int:id>/', views.delete_todo, name="delete_todo")
+    path('delete_todo/<int:id>/', views.delete_todo, name="delete_todo"),
+    path('update/<int:id>/', views.update, name="update")
 ]
--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,20 @@ def index(request):
     }
     return render(request, 'home.html', context)
 
+def update(request, id):
+    todos_list = Todo.objects.all()
+    todo_to_update = Todo.objects.get(id=id)
+    todo_form = TodoForm(request.POST or None, instance=todo_to_update or None)
+    if todo_form.is_valid():
+        todo_form.save()
+        todo_form = TodoForm()
+
+    context = {
+        "todos_list": todos_list,
+        "form": todo_form
+    }
+    return render(request, 'home.html', context)
+
 
 def delete_todo(request, id):
     todo = Todo.objects.get(id=id)
